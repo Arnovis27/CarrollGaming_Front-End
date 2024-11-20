b0ba210afa70a8f1d8386d8cef3b6a4795989c2c
Arnovis27 <arnovisdee@hotmail.com>
2024-11-20 09:57:18 -0500

    desplazamiento suave para ahorra codigo

--- js/main.js
+++ js/main.js
@@ -65,5 +65,19 @@
             }
         }
     });
+
+    document.querySelectorAll('.smooth-scroll').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            // Animación hacia abajo
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
     
 })(jQuery);
